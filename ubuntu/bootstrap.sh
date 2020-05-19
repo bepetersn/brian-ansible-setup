038dd1f2d86b8d9c3a4830da4e9dd348f6b83c1b
Brian E. Peterson <bepetersn@gmail.com>
2020-05-19 09:43:54 -0600

    Path to playbook & don't upgrade

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -2,9 +2,8 @@
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-add-repository --yes --update ppa:git-core/ppa
 sudo apt update
-sudo apt upgrade -y
 sudo apt install -y ansible
 sudo apt install -y git 
 git clone https://github.com/bepetersn/brian-ansible-setup .dev-env
 cd .dev-env
-ansible-playbook playbook.yml -vv --ask-become-pass
+ansible-playbook ubuntu/playbook.yml -vv --ask-become-pass
