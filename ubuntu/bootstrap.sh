fceaeff0e4b22530b075977dbaaa0d951068b742
Brian E. Peterson <bepetersn@gmail.com>
2020-05-19 14:28:36 -0600

    Docker, list format, simpler

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -3,7 +3,6 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-add-repository -y ppa:git-core/ppa
 sudo apt install -y ansible
 sudo apt install -y git 
-rm -rf .dev-env
+CODE=~/Desktop/Code
 git clone https://github.com/bepetersn/brian-ansible-setup .dev-env
-cd .dev-env
-ansible-playbook ubuntu/playbook.yml -vv --ask-become-pass
+ansible-playbook .dev-env/ubuntu/playbook.yml -vv --ask-become-pass
