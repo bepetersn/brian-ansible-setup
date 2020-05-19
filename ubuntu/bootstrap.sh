5258a5bfab16bd28e0850946e3c7abd170cf5419
Brian E. Peterson <bepetersn@gmail.com>
2020-05-19 14:30:26 -0600

    Simpler local paths

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -3,6 +3,5 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-add-repository -y ppa:git-core/ppa
 sudo apt install -y ansible
 sudo apt install -y git 
-CODE=~/Desktop/Code
-git clone https://github.com/bepetersn/brian-ansible-setup .dev-env
-ansible-playbook .dev-env/ubuntu/playbook.yml -vv --ask-become-pass
+git clone https://github.com/bepetersn/brian-ansible-setup ~/.dev-env
+ansible-playbook ~/.dev-env/ubuntu/playbook.yml -vv --ask-become-pass
